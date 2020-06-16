d0e6eeda13a4750da8118a8905297b27fd868c84
Md. Kamruzzaman <mkzamanalive@gmail.com>
2020-06-16 18:06:34 +0600

    Codeforces Round #644 (Div. 3)

---
+++ CodeForces/Codeforces Round 644 (Div. 3)/A. Minimal Square/A. Minimal Square/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace A._Minimal_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = Convert.ToInt32(Console.ReadLine());
+            for(int i =0; i < cases; i++)
+            {
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), (item) => Convert.ToInt32(item));
+                int a = numbers[0];
+                int b = numbers[1];
+
+                if (b < a)
+                {
+                    b *= 2;
+                }
+                else
+                {
+                    a *= 2;
+                }
+
+                int result = a > b ? a * a : b * b;
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ CodeForces/Codeforces Round 644 (Div. 3)/B. Honest Coach/B. Honest Coach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace B._Honest_Coach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while (cases-- > 0)
+            {
+                int number_of_athlets = int.Parse(Console.ReadLine());
+                int[] athlets = Array.ConvertAll(Console.ReadLine().Split(' '), (item) => Convert.ToInt32(item));
+                Array.Sort(athlets);
+
+                int answer = Int32.MaxValue;
+                for(int i = 1; i<number_of_athlets; i++)
+                {
+                    int temp = athlets[i] - athlets[i - 1];
+                    if (temp < answer)
+                    {
+                        answer = temp;
+                    }
+                }
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
