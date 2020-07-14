cb8e629edef1ad33a91026a951689b47f2ca4bfd
mkzaman <mkzamanalive@gmail.com>
2020-07-14 23:03:35 +0600

    Round 655 div 2 A and B solved

---
+++ CodeForces/Codeforces Round 655 (Div. 2)/Problem A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while (cases-- > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                for(int i=0; i<a;i++)
+                {
+                    if(i != a-1)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("1");
+                    }
+                    
+                }
+
+            }
+        }
+    }
+}
---
+++ CodeForces/Codeforces Round 655 (Div. 2)/Problem B/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while (cases-- > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a % 2 == 0)
+                {
+                    int b = a / 2;
+                    Console.WriteLine(b + " " + b);
+                }
+                else
+                {
+                    int x = (int)Math.Sqrt(a);
+                    int min = Int32.MaxValue;
+                    for (int y = 3; y <= x; y += 2)
+                    {
+                        int n = a - y;
+                        if(n%y == 0 && min>n)
+                        {
+                            min = n;
+                        }
+
+                        int p = a / y;
+                        int m = a - p;
+                        if(m % p == 0 && min>m)
+                        {
+                            min = m;
+                        }
+                    }
+                    if(min == Int32.MaxValue)
+                    {
+                        Console.WriteLine("1 " + (a - 1));
+                    }
+                    else
+                    {
+                        Console.WriteLine((a - min) + " " + min);
+                    }
+                }
+            }
+        }
+    }
+}
