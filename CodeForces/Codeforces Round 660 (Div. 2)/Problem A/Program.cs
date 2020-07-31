189c9e5f191bc61b23397f953f55b7fd918184c7
Md. Kamruzzaman <mkzamanalive@gmail.com>
2020-07-31 10:53:10 +0600

    660 div 2 A & B solved

---
+++ CodeForces/Codeforces Round 660 (Div. 2)/Problem A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while (cases-- > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if(n <= 30)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                    if(n==36)
+                    {
+                        Console.WriteLine("5 6 10 15");
+                    }
+                    else if(n==40)
+                    {
+                        Console.WriteLine("6 10 15 9");
+                    }
+                    else if(n==44)
+                    {
+                        Console.WriteLine("6 7 10 21");
+                    }
+                    else
+                    {
+                        Console.WriteLine("6 10 14 " + (n - 30));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CodeForces/Codeforces Round 660 (Div. 2)/Problem B/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while(cases-- > 0)
+            {
+                int x = int.Parse(Console.ReadLine());
+                StringBuilder sb = new StringBuilder();
+
+                int a = x % 4 == 0 ? x / 4 : x / 4 + 1;
+
+                int b = x - a;
+
+                for (int i = 0; i < b; i++)
+                {
+                    sb.Append('9');
+                }
+                for (int i = 0; i < a; i++)
+                {
+                    sb.Append('8');
+                }
+
+                Console.WriteLine(sb);
+            }
+        }
+    }
+}
