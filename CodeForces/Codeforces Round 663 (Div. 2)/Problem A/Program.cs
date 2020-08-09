c13601f566ee491a9f1cd1a2eb3c49bf908feee3
mkzaman <mkzamanalive@gmail.com>
2020-08-10 00:49:27 +0600

    Codeforces Round #663 (Div. 2) A & B solved

---
+++ CodeForces/Codeforces Round 663 (Div. 2)/Problem A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while (cases-- > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                for(int i=1; i<=a;i++)
+                {
+                    if(i == a)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    else
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ CodeForces/Codeforces Round 663 (Div. 2)/Problem B/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            while (cases-- > 0)
+            {
+                int n, m;
+                int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), (item) => Convert.ToInt32(item));
+                n = input[0];
+                m = input[1];
+
+                int count = 0;
+
+                for(int i = 1; i<=n; i++)
+                {
+                    string s = Console.ReadLine();
+
+                    if(i == n)
+                    {
+                        for(int j=0; j<m-1; j++)
+                        {
+                            if(s[j] == 'D')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(s[m-1] == 'R')
+                        {
+                            count++;
+                        }
+                    }
+
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
