b8ec9706a11c14323a1e8a646dbaa70a87ec3ac3
mkzaman <mkzamanalive@gmail.com>
2021-12-23 14:24:45 +0600

    Day 1 and Day 2 of AoC

---
+++ AoC/2021/Day 1 Sonar Sweep/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string[] lines = File.ReadAllLines("input.txt");
+List<int> depth = new List<int>();
+int count = 0;
+foreach (string line in lines)
+{
+    depth.Add(int.Parse(line));
+}
+
+for(int i = 1; i < depth.Count; i++)
+{
+    if(depth[i] > depth[i-1])
+    {
+        count++;
+    }
+}
+
+count = 0;
+for (int i = 0; i < depth.Count; i++)
+{
+    if(i+3 >= depth.Count)
+    {
+        break;
+    }
+    if(depth[i] + depth[i+1] + depth[i+2] < depth[i+1] + depth[i + 2] + depth[i + 3])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+    
---
+++ AoC/2021/Day 2 Dive!/Program.cs
@@ -0,0 +1,59 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+Int64 horizontal = 0;
+Int64 vertical = 0;
+
+string command =  string.Empty;
+int value;
+Int64 aim = 0;
+
+//first part
+foreach (string line in lines)
+{
+    var input = line.Split(' ');
+    command = input[0];
+    value = Convert.ToInt32(input[1]);
+
+    if (string.Equals(command, "forward"))
+    {
+        horizontal += value;
+    }
+    else if (string.Equals(command, "up"))
+    {
+        vertical -= value;
+    }
+    else
+    {
+        vertical += value;
+    }
+}
+
+Console.WriteLine(horizontal * vertical);
+
+horizontal = 0;
+vertical = 0;
+
+//second part
+foreach (string line in lines)
+{
+    var input = line.Split(' ');
+    command = input[0];
+    value = Convert.ToInt32(input[1]);
+    if (string.Equals(command, "forward"))
+    {
+        horizontal += value;
+        vertical += aim * value;
+    }
+    else if (string.Equals(command, "up"))
+    {
+        aim -= value;
+    }
+    else
+    {
+        aim += value;
+    }
+}
+
+Console.WriteLine(horizontal*vertical);
+
+
